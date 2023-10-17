734be3a523cd97a4d8376fd2f772c2d5dee1a379
Timmie Faye <timmiedewan@gmail.com>
2023-10-17 06:33:02 -0700

    Display pet objects

--- js/script.js
+++ js/script.js
@@ -32,6 +32,25 @@ const francine = createPet("Francine", "turtle");
 // console.log(sora, clover, baxter, cleo, francine);
 // clover.sleep();
 // baxter.play();
-
 // console.log(clover, baxter);
+clover.isTired = 8;
+francine.isTired = 9
 
+const allPets = [sora, clover, baxter, cleo, francine];
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+  for (let pet of petArray) {
+    let status = "ready for play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+}
+
+statusButton.addEventListener("click", function() {
+  showPets(allPets);
+})
